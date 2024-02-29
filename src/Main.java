654a7f39e8467a11ed61c615958d613cbedbaeb1
MariMax <maxim.prohachev@yandex.ru>
2024-02-29 14:51:28 +0300

    ferst commit

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,17 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int initial_balance = 100; // изначальный баланс
+        int initialBalance = 100; // изначальный баланс
         int refill = 1100; //пополнение счета
         int bonus = refill / 100;// бонус за каждые 100 рублей
         if (refill > 1000) {
-            int amount = initial_balance + refill + bonus;
+            int amount = initialBalance + refill + bonus;
             System.out.println("Итоговая сумма-" + amount + "руб.; Бонусы-" + bonus + "руб.");
 
         } else {
-            int amount1 = initial_balance + refill;
+            int amount1 = initialBalance + refill;
             System.out.println("Итоговая сумма-" + amount1 + "руб.; Бонусы - 0 руб.");
-                   }
+        }
     }
 
 }
